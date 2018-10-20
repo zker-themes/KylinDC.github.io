17ebde2253c1e3b558807f857c16837f17aa8aad
KylinDC <inlovingdays@gmail.com>
2018-10-20 18:42:48 +0800

    Site updated: 2018-10-20 18:42:43

--- js/main.js
+++ js/main.js
@@ -1,1 +1,106 @@
-if($.prototype.justifiedGallery){var options={rowHeight:140,margins:4,lastRow:"justify"};$(".article-gallery").justifiedGallery(options)}$(document).ready(function(){if($("#header > #nav > ul > .icon").click(function(){$("#header > #nav > ul").toggleClass("responsive")}),$(".post").length){var i=$("#menu"),o=$("#menu-icon, #menu-icon-tablet");if(o.click(function(){return"hidden"===i.css("visibility")?(i.css("visibility","visible"),o.addClass("active")):(i.css("visibility","hidden"),o.removeClass("active")),!1}),i.length&&$(window).on("scroll",function(){var o=$("#menu > #nav").offset().top;"hidden"!==i.css("visibility")&&o<50?$("#menu > #nav").show():"hidden"!==i.css("visibility")&&o>100&&$("#menu > #nav").hide(),!$("#menu-icon").is(":visible")&&o<50?($("#menu-icon-tablet").show(),$("#top-icon-tablet").hide()):!$("#menu-icon").is(":visible")&&o>100&&($("#menu-icon-tablet").hide(),$("#top-icon-tablet").show())}),$("#footer-post").length){var e=0;$(window).on("scroll",function(){var i=$(window).scrollTop();i>e?$("#footer-post").hide():$("#footer-post").show(),e=i,$("#nav-footer").hide(),$("#toc-footer").hide(),$("#share-footer").hide(),i<50?($("#actions-footer > ul > #top").hide(),$("#actions-footer > ul > #menu").show()):i>100&&($("#actions-footer > ul > #menu").hide(),$("#actions-footer > ul > #top").show())})}}});
+/**
+ * Sets up Justified Gallery.
+ */
+if (!!$.prototype.justifiedGallery) {
+  var options = {
+    rowHeight: 140,
+    margins: 4,
+    lastRow: 'justify'
+  };
+  $('.article-gallery').justifiedGallery(options);
+}
+
+
+$(document).ready(function() {
+
+  /**
+   * Shows the responsive navigation menu on mobile.
+   */
+  $("#header > #nav > ul > .icon").click(function() {
+    $("#header > #nav > ul").toggleClass("responsive");
+  });
+
+
+  /**
+   * Controls the different versions of  the menu in blog post articles 
+   * for Desktop, tablet and mobile.
+   */
+  if ($(".post").length) {
+    /**
+     * Display the menu if the menu icon is clicked.
+     */
+    var menu = $("#menu");
+    var menu_icon = $("#menu-icon, #menu-icon-tablet");
+    menu_icon.click(function() {
+      if (menu.css('visibility') === 'hidden') {
+        menu.css("visibility", "visible");
+        menu_icon.addClass('active');
+      } else {
+        menu.css("visibility", "hidden");
+        menu_icon.removeClass('active');
+      }
+      return false;
+    });
+
+    /**
+     * Add a scroll listener to the menu to hide/show the navigation links.
+     */
+    if (menu.length) {
+      $(window).on('scroll', function() {
+        var topDistance = $("#menu > #nav").offset().top;
+
+        // hide only the navigation links on desktop
+        if (menu.css('visibility') !== 'hidden' && topDistance < 50) {
+          $("#menu > #nav").show();
+        } else if (menu.css('visibility') !== 'hidden' && topDistance > 100) {
+          $("#menu > #nav").hide();
+        }
+
+        // on tablet, hide the navigation icon as well and show a "scroll to top
+        // icon" instead
+        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+          $("#menu-icon-tablet").show();
+          $("#top-icon-tablet").hide();
+        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+          $("#menu-icon-tablet").hide();
+          $("#top-icon-tablet").show();
+        }
+      });
+    }
+
+    /**
+     * Show mobile navigation menu after scrolling upwards,
+     * hide it again after scrolling downwards.
+     */
+    if ($( "#footer-post").length) {
+      var lastScrollTop = 0;
+      $(window).on('scroll', function() {
+        var topDistance = $(window).scrollTop();
+
+        if (topDistance > lastScrollTop){
+          // downscroll -> show menu
+          $("#footer-post").hide();
+        } else {
+          // upscroll -> hide menu
+          $("#footer-post").show();
+        }
+        lastScrollTop = topDistance;
+
+        // close all submenu's on scroll
+        $("#nav-footer").hide();
+        $("#toc-footer").hide();
+        $("#share-footer").hide();
+
+        // show a "navigation" icon when close to the top of the page, 
+        // otherwise show a "scroll to the top" icon
+        if (topDistance < 50) {
+          $("#actions-footer > ul > #top").hide();
+          $("#actions-footer > ul > #menu").show();
+        } else if (topDistance > 100) {
+          $("#actions-footer > ul > #menu").hide();
+          $("#actions-footer > ul > #top").show();
+        }
+      });
+    }
+  }
+});
