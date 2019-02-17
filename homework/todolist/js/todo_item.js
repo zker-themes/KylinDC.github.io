83781a134576ff4a6b3a5e83ab27d2e79bb86bbd
KylinDC <inlovingdays@gmail.com>
2019-02-17 23:47:56 +0800

    Site updated: 2019-02-17 23:47:53

--- homework/todolist/js/todo_item.js
+++
@@ -1,7 +0,0 @@
-class TodoItem {
-  constructor(content, isCompleted = false, isDeleted = false) {
-    this.content = content;
-    this.isCompleted = isCompleted;
-    this.isDeleted = isDeleted;
-  }
-}
